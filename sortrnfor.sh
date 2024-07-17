a3fe0c4382fd69e9292d3eb1c2c13312d3ed52a5
jec423 <128650277+jec423@users.noreply.github.com>
2024-07-17 11:11:27 -0400

    Update sortrnfor.sh

--- sortrnfor.sh
+++ sortrnfor.sh
@@ -10,7 +10,7 @@
 #SBATCH --cpus-per-task=24
 
 export PATH=$PATH:/home/jec423/miniconda3/bin
-cd /scratch/jec423/trimmed_files/profemale
+cd /scratch/jec423/fastq/trimmed_files
 
 sortmerna --ref /scratch/jec423/rRNA_databases_v4/smr_v4.3_default_db.fasta --reads 27_Female_pro-estrus_S14_R1_001_val_1.fq --reads 27_Female_pro-estrus_S14_R2_001_val_2.fq --workdir /scratch/jec423/trimmed_files/profemale/sortrna/femalepro27 --threads 24 --fastx
 
